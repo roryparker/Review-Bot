21017d3eaf7aabb8d55aa205483e45f19bde30fa
unknown <rorysethparker@protonmail.com>
2021-11-04 14:02:03 -0400

    added icons

--- includes/header.php
+++ includes/header.php
@@ -11,15 +11,37 @@ else {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Truth</title>
+
+    <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="assets/css/js/bootstrap.js"></script>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
 </head>
+
 <body>
 
-ddddddddddddddddddddddddddddddddddddddd
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php"> KWORKR </a> 
+            <!-- img src="assets/images/logo" -->
+        </div>
+
+        <nav>
+            <a href="#"> <i class="fa-solid fa-house"></i> </a>
+            <a href="#"> <i class="fa-solid fa-user-group"></i> </a>
+            <a href="#"> <i class="fa-solid fa-comment"></i> </a>
+            <a href="#"> <i class="fa-solid fa-people-arrows-left-right"></i> </a>
+            <a href="#"> <i class="fa-regular fa-face-pouting"></i> </a>
+        </nav>
+    </div>
--- register.php
+++ register.php
@@ -8,6 +8,7 @@ require 'includes/form_handlers/login_handler.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,7 +23,7 @@ require 'includes/form_handlers/login_handler.php';
 
 <body>
 
-        <?php                         
+    <?php                         
 
             // if(isset($_POST['register_button'])) {   // Code block doesn't work
             //     echo '
@@ -37,25 +38,25 @@ require 'includes/form_handlers/login_handler.php';
             //     ';
             // }
 
-        ?> 
+        ?>
 
     <div class="wrapper">
 
         <div class="login_box">
-            
-            <div class="login_header"> 
+
+            <div class="login_header">
                 <h1> Welcome to KWORKR </h1>
                 <h2>The review based HR System </h2>
 
             </div>
-            
-            <div id="first"> 
 
-                <form action="register.php" method="POST"> 
+            <div id="first">
+
+                <form action="register.php" method="POST">
                     <input type="email" name="log_email" placeholder="Email Address" value=" <?php 
                         if (isset($_SESSION['log_email'])) { 
                             echo $_SESSION['log_email']; 
-                        } ?>" required> 
+                        } ?>" required>
                     <br>
                     <input type="password" name="log_password" placeholder="Password">
                     <br>
@@ -65,35 +66,48 @@ require 'includes/form_handlers/login_handler.php';
                     <br>
                     <a href="#" id="signin" class="signin">Already have an account, then sign in!</a>
                 </form>
-    
+
             </div>
 
- 
+
             <div id="second">
 
                 <form action="register.php" method="POST">
 
-                    <input type="text" name="register_first_name" placeholder="First Name" value="<?php if (isset($_SESSION['register_first_name'])) { echo $_SESSION['register_first_name']; } ?>" required>
+                    <input type="text" name="register_first_name" placeholder="First Name"
+                        value="<?php if (isset($_SESSION['register_first_name'])) { echo $_SESSION['register_first_name']; } ?>"
+                        required>
                     <br>
                     <?php if(in_array("Your first name must be between 2 and 25 characters.<br>", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
 
-                    <input type="text" name="register_last_name" placeholder="Last Name" value="<?php if (isset($_SESSION['register_last_name'])) { echo $_SESSION['register_last_name']; } ?>" required>
+                    <input type="text" name="register_last_name" placeholder="Last Name"
+                        value="<?php if (isset($_SESSION['register_last_name'])) { echo $_SESSION['register_last_name']; } ?>"
+                        required>
                     <br>
                     <?php if(in_array("Your last name must be between 2 and 25 characters.<br>", $error_array)) echo "Your last name must be between 2 and 25 characters.<br>"; ?>
-                    
-                    <input type="email" name="register_email" placeholder="Email Address" value="<?php if (isset($_SESSION['register_email'])) { echo $_SESSION['register_email']; } ?>"required>
-                    <br>    
-                    <input type="email" name="register_email_confirmation" placeholder="Confirm Email Address" value="<?php if (isset($_SESSION['register_email_confirmation'])) { echo $_SESSION['register_email_confirmation']; } ?>"required>
+
+                    <input type="email" name="register_email" placeholder="Email Address"
+                        value="<?php if (isset($_SESSION['register_email'])) { echo $_SESSION['register_email']; } ?>"
+                        required>
+                    <br>
+                    <input type="email" name="register_email_confirmation" placeholder="Confirm Email Address"
+                        value="<?php if (isset($_SESSION['register_email_confirmation'])) { echo $_SESSION['register_email_confirmation']; } ?>"
+                        required>
                     <br>
 
                     <?php if(in_array("This email address is already in use<br>", $error_array)) echo "This email address is already in use<br>";
                         if(in_array("This email address is using an invalid format.<br>", $error_array)) echo "This email address is using an invalid format.<br>";
                         if(in_array("The email address does not match.<br>", $error_array)) echo "The email address does not match.<br>"; ?>
 
-                    
-                    <input type="password" name="register_password" autocomplete="on" placeholder="Password" value="<?php if (isset($_SESSION['register_password'])) { echo $_SESSION['register_password']; } ?>"required>
-                    <br>    
-                    <input type="password" name="register_password_confirmation" autocomplete="on" placeholder="Confirm Password" value="<?php if (isset($_SESSION['register_password_confirmation'])) { echo $_SESSION['register_password_confirmation']; } ?>" required>
+
+                    <input type="password" name="register_password" autocomplete="on" placeholder="Password"
+                        value="<?php if (isset($_SESSION['register_password'])) { echo $_SESSION['register_password']; } ?>"
+                        required>
+                    <br>
+                    <input type="password" name="register_password_confirmation" autocomplete="on"
+                        placeholder="Confirm Password"
+                        value="<?php if (isset($_SESSION['register_password_confirmation'])) { echo $_SESSION['register_password_confirmation']; } ?>"
+                        required>
                     <br>
                     <?php if(in_array("Your password must be between 5 and 30 characters.<br>", $error_array)) echo "Your password must be between 5 and 30 characters.<br>";
                         if(in_array("Your password can only contain letters or numbers.<br>", $error_array)) echo "Your password can only contain letters or numbers.<br>";
@@ -106,12 +120,13 @@ require 'includes/form_handlers/login_handler.php';
                     <a href="#" id="signup" class="signup">Need an account? Register here!</a>
 
                 </form>
-            
+
             </div>
-            
+
         </div>
 
     </div>
-    
+
 </body>
+
 </html>
