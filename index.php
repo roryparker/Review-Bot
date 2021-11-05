d1558f60d1e77833b21268aff29c692df2d90f99
roryparker <rorysethparker@gmail.com>
2021-11-05 18:35:33 -0400

    updated

--- index.php
+++ index.php
@@ -6,6 +6,21 @@ include("includes/header.php");
 
 <div class="user_details column">
     <a href="#" img src="<?php echo $user['profile_pic']; ?>"> </a>
+
+    <div class="user_details_left_right">
+        <a href="#">
+
+            <?php 
+                echo $user['first_name'] . " " . $user['last_name'];
+            ?>
+        </a>
+        <br>
+
+        <?php
+            echo "Posts: " . $user['num_posts']. "<br>";   
+            echo "Likes: " . $user['num_likes'];
+        ?>
+    </div>
 </div>
 
 </div>
