32116e1220b128a204ef76b7e904709c9f8645d7
roryparker <rorysethparker@gmail.com>
2021-11-06 14:35:33 -0400

    fixed the login issue. Display issues are present

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -6,14 +6,16 @@ if (isset($_POST['login_button'])) {
     $_SESSION['log_email'] = $email; // Stores email in session variables
     $password = md5($_POST['log_password']); // Get password
 
-    $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email AND password='$password'");
-    // $check_login_query = mysqli_num_rows($check_database_query); -------------> Pushes an error due to mysqli_num_rows and php version ???
+    $check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND password='$password'");
+    $check_login_query = mysqli_num_rows($check_database_query); // -------------> Pushes an error due to mysqli_num_rows and php version ???
+    // $check_login_query = $check_database_query -> mysqli_num_rows(); // -------------> Pushes an error due to mysqli_num_rows and php version ???
+    // $check_login_query = mysqli_query($con, $check_database_query) or die("Error");
 
     if ($check_login_query == 1) {
         $row = mysqli_fetch_array($check_database_query);
         $username = $row['username'];
 
-        $user_closed_query = mysqli_query($con, "SELECT  FROM users WHERE email='$email' AND user_closed='yes'" ); // reopens closed account
+        $user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND user_closed='yes'" ); // reopens closed account
         if(mysqli_num_rows($user_closed_query) == 1) {
             $reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'" );
         }
--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,9 @@ else {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Truth</title>
+    <title>KOWRKR, EMPLOYR, ASS|_|
+                                 
+    </title>
 
     <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -33,7 +35,7 @@ else {
 
     <div class="top_bar">
         <div class="logo">
-            <a href="index.php"> KWORKR </a> 
+            <a href="index.php"> KOWRKR </a> 
             <!-- img src="assets/images/logo" -->
         </div>
 
