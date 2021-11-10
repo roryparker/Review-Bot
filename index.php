f17c0705afff2094f3659bf06e35eec703153502
roryparker <rorysethparker@gmail.com>
2021-11-09 20:46:49 -0500

    fixed several display errors

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 include("includes/header.php");
-// session_destroy();              // destroy session
+include("includes/form_handlers/register_handler.php");
 
 ?>
 
     <div class="user_details column">
-        <a href="#" img src="<?php echo $user['profile_pic']; ?>"> </a>
+        <a href="#" img src="<?php echo isset($user['profile_pic']); ?>"> </a>
 
         <div class="user_details_left_right">
             <a href="#">
 
                 <?php 
-                    echo $user['first_name'] . " " . $user['last_name'];
+                    echo isset($user['first_name']) . " " . isset($user['last_name']);
                 ?>
             </a>
             <br>
 
             <?php
-                echo "Posts: " . $user['num_posts']. "<br>";   
-                echo "Likes: " . $user['num_likes'];
+                echo "Posts: " . isset($user['num_posts']). "<br>";   
+                echo "Likes: " . isset($user['num_likes']);
             ?>
         </div>
     </div>
@@ -26,7 +26,7 @@ include("includes/header.php");
     <div class="main_column column"> 
         <form class="post_form" action="index.php" method="POST">
             <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
-            <input type="submit" name="post" id="post_button" value="Post"
+            <input type="submit" name="post" id="post_button" value="Post">
         </form>
     </div>
 
