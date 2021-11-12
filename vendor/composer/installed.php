8d7b2aaff485037805e9592b7dbce25f1d5633ab
roryparker <rorysethparker@protonmail.com>
2021-11-11 20:04:26 -0500

    added composer and phpcs fixer for PHP syntax issues

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1c672ed8c9058156d266c9086e5e24d58ab1f811',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1c672ed8c9058156d266c9086e5e24d58ab1f811',
+            'dev_requirement' => false,
+        ),
+        'php-cs-fixer/diff' => array(
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-cs-fixer/diff',
+            'aliases' => array(),
+            'reference' => '29dc0d507e838c4580d018bd8b5cb412474f7ec3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
